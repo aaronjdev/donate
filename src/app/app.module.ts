74a55849bd995a1bd8a869756be35ba85d616ded
aaron <jonesaaro@gmail.com>
2018-08-16 11:40:24 -0500

    committing all files for this project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DonateBoxComponent } from './donate-box/donate-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DonateBoxComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/donate-box/donate-box.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donate-box',
+  templateUrl: './donate-box.component.html',
+  styleUrls: ['./donate-box.component.css']
+})
+export class DonateBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  goal: number = 5000;
+  totalDonated: number = 0;
+  amountToGo: number = this.goal;
+  donors: number = 0;
+  progress:number = 0;
+
+
+  onGive(value) {
+    value = parseInt(value); 
+    if(this.checkAmount(value)){
+      //console.log("hello " + value);
+      this.handleDonation(value);
+
+    }else{
+      alert("donation is under $5");
+    }
+
+  //lastly, clear the box
+  }
+  checkAmount(amount){
+    if(amount >=5){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  handleDonation(value){
+    this.totalDonated += value;
+    this.amountToGo = this.goal-this.totalDonated;
+    this.donors++;
+    this.progress = Math.round((this.totalDonated/5000) * 100);
+
+  }
+
+
+}
